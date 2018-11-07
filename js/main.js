24f5576d7649adb4a7f293a19463b73d1db4509e
Shalabi <m_shalabi2@tss-hr420-dm27.fc.ca>
2018-11-07 13:38:53 -0500

    added files to repo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+TweenMax.from("#greenline_7_", 3, {scale:1, rotation:180});
+
+TweenMax.to("#orangeline", 4, {scale:1, rotation:360});
+
+TweenMax.from("#blueline_1_", 3, {scale:1, rotation:220});
+
+TweenMax.from("#blueline_10_", 3, {scale:1, rotation:220});
